3361ef72eae9654706bdb788ff4bb823a3868822
John Hutchins <john_hutchins@comcast.net>
2020-05-27 20:56:01 -0600

    bootstrap, popper, auto reload added

--- main.js
+++ main.js
@@ -2,6 +2,25 @@ const {app, BrowserWindow} = require('electron')
     const url = require("url");
     const path = require("path");
 
+
+    require('electron-reload')(__dirname, {
+      // Note that the path to electron may vary according to the main file
+      electron: require(`${__dirname}/node_modules/electron`)
+    });
+
+    // require('electron-reload')(
+    //   [
+    //     __dirname,
+    //     '${__dirname}/../../html/**/*.html',
+    //     '${__dirname}/../../js/**/*.js',
+    //     '${__dirname}/../../ts/**/*.ts',
+    //     '${__dirname}/../../css/**/*.css'
+    //   ],
+    //    {
+    //   electron: require('${__dirname}/../../node_modules/electron')
+    // })
+
+
     let mainWindow
 
     function createWindow () {
@@ -13,13 +32,7 @@ const {app, BrowserWindow} = require('electron')
         }
       })
 
-      mainWindow.loadURL(
-        url.format({
-          pathname: path.join(__dirname, `/dist/index.html`),
-          protocol: "file:",
-          slashes: true
-        })
-      );
+      mainWindow.loadURL('http://localhost:4200/');
       // Open the DevTools.
       mainWindow.webContents.openDevTools()
 
